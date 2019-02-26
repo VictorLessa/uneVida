49283522fb187bc5e74f778f7adb10c37c9cca3c
Victor Lessa <victor.lessa@acheifarmacias.com.br>
2019-02-25 22:41:23 -0300

    responsivo no video

--- src/components/Section2.vue
+++ src/components/Section2.vue
@@ -2,10 +2,17 @@
   <div>
     <v-container>
       <v-layout wrap row>
-        <v-flex lg8>
-          <iframe id="ytplayer" class="playYoutube" type="text/html" src="https://www.youtube.com/embed/?autoplay=1&list=PLB_vaUBxT31Wx_drOfN24kGpWEYk-tvMw&listType=playlist&playsinline=1" frameborder="0" allowfullscreen/>
+        <v-flex>
+          <iframe
+            id="ytplayer"
+            class="playYoutube"
+            type="text/html"
+            src="https://www.youtube.com/embed/?autoplay=1&list=PLB_vaUBxT31Wx_drOfN24kGpWEYk-tvMw&listType=playlist&playsinline=1"
+            frameborder="0"
+            allowfullscreen
+          />
         </v-flex>
-        <v-flex lg4>
+        <!-- <v-flex lg4>
           <v-layout row wrap>
             <v-flex lg12 class="grid">
               <VuePerfectScrollbar class="scroll-area" v-once :settings="settings">
@@ -23,7 +30,7 @@
               </VuePerfectScrollbar>
             </v-flex>
           </v-layout>
-        </v-flex>
+        </v-flex>-->
       </v-layout>
     </v-container>
     <v-container>
@@ -34,7 +41,7 @@
 </template>
 
 <script>
-import VuePerfectScrollbar from 'vue-perfect-scrollbar'
+import VuePerfectScrollbar from "vue-perfect-scrollbar";
 // import Slider from "./Slider";
 export default {
   name: "Section2",
@@ -44,62 +51,84 @@ export default {
         maxScrollbarLength: 60
       },
       active: [false, false, false, false]
-    }
+    };
   },
   components: {
     VuePerfectScrollbar
     // Slider
   },
   watch: {
-    active (val) {
-      console.log('watch')
-      this.active = val
+    active(val) {
+      console.log("watch");
+      this.active = val;
     }
   },
   methods: {
-    card_bottom (index) {
+    card_bottom(index) {
       for (let i = 0; i < this.active.length; i++) {
-        this.active[i] = false
+        this.active[i] = false;
       }
-      this.active[index] = 1
+      this.active[index] = 1;
     }
   }
-}
+};
 </script>
 <style scoped lang="stylus">
-i = !important
-.active
-  background red i
-.scroll-area
+i = !important;
+
+.active {
+  background: red i;
+}
+
+.scroll-area {
   position: relative;
   margin: auto;
   width: 400px;
   height: 405px;
-  background black
-  border-radius 5px
-.playYoutube
+  background: black;
+  border-radius: 5px;
+}
+
+.playYoutube {
   width: 720px;
   height: 405px;
-.description
-  text-align left
-.card
-  width 94%
-  margin 5px
-  background black
-  color white
-  border-bottom: 1px solid #f4f4f4
-  &:hover
-    background #303030
+}
 
-@media (max-width: 560px)
-  .playYoutube
-    width 100%
-    height 185px
-  .scroll-area
+.description {
+  text-align: left;
+}
+
+.card {
+  width: 94%;
+  margin: 5px;
+  background: black;
+  color: white;
+  border-bottom: 1px solid #f4f4f4;
+
+  &:hover {
+    background: #303030;
+  }
+}
+
+@media (max-width: 560px) {
+  .playYoutube {
+    width: 100%;
+    height: 185px;
+  }
+
+  .scroll-area {
     position: relative;
     margin: auto;
     width: auto;
     height: 405px;
-    background black
-    border-radius 5px
+    background: black;
+    border-radius: 5px;
+  }
+}
+
+@media (max-width: 768px) {
+  .playYoutube {
+    width: 100%;
+  }
+}
 </style>
