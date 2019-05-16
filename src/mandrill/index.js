50b6e5af562937b64bc742c64cdb1ecc2d45f878
victor <victor.lessa@acheifarmacias.com.br>
2019-05-15 23:45:38 -0300

    envio de email

--- src/mandrill/index.js
+++ src/mandrill/index.js
@@ -1,54 +1,59 @@
-const nodemailer = require('nodemailer')
-
-const MailChimp = require('mailchimp-api-v3')
-
-class nodeMailer {
-  constructor () {
-    console.log(process.env.NOREPLY_HOST)
-    this.transporter = nodemailer.createTransport({
-      host: process.env.NOREPLY_HOST,
-      port: process.env.NOREPLY_PORT,
-      secure: process.env.NOREPLY_IS_SECURE, // true for 465, false for other ports
-      // autentication
-      auth: {
-        user: process.env.NOREPLY_EMAIL, // generated ethereal user
-        pass: process.env.NOREPLY_PASSWORD// generated ethereal password
+const Axios =  require('axios')
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill('RU0Fe65llH9ogd5jywslDA');
+class Mandrill {
+  async sendEmail () {
+    var message = {
+      "html": "<p>Example HTML content</p>",
+      "text": "Example text content",
+      "subject": "example subject",
+      "from_email": "victor.lessa@universidadeparavida.com.br",
+      "from_name": "Example Name",
+      "to": [{
+              "email": "victor.lessa@gmail.com",
+              "name": "Recipient Name",
+              "type": "to"
+          }],
+      "headers": {
+          "Reply-To": "victor.lessa@universidadeparavida.com.br"
       }
-    })
-    this.mailChimp = new MailChimp('adb50a4fb0f2e8d35fd56d0547a8aa7b-us20')
-  }
-
-  async mailChimpLandPage () {
-    try {
-      await this.mailChimp.post(`/lists/${listID}/members`, {
-        email_address: payload,
-        status: 'subscribed',
-        merge_fields: {
-          FNAME: ' ',
-          LNAME: ' '
-        }
-      })
-    } catch (error) {
-
     }
-
-    sendEmail()
-  }
-
-  async sendEmail (listID) {
-    let message = {
-      from: 'Favor Não Responder',
-      to: 'victordsgnr@gmail.com',
-      subject: 'Message title',
-      text: 'Plaintext version of the message',
-      html: '<p>tste</p>' ,//'<p>HTML version of the message</p>',
-      replyTo: '',
-      encoding: ''
-    };
-    await this.transporter.sendMail(message)
+    var async = false;
+    var ip_pool = "Main Pool";
+    var send_at = "11/03/2019";
+    mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function (result) {
+      console.log(result)
+    })
+    // let config = {
+    //   method: 'post',
+    //   url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+    //   data : {
+    //     key: 'NBoSRI0OpI4ZJ-8odxulbA',
+    //     message: {
+    //       "html": "<p>Example HTML content</p>",
+    //       "text": "Example text content",
+    //       "subject": "example subject",
+    //       "from_email": "admin@universidadeparavida.com.br",
+    //       "from_name": "Example Name",
+    //       "to": [
+    //           {
+    //               "email": "victordsgnr@gmail.com",
+    //               "name": "Recipient Name",
+    //               "type": "to"
+    //           }
+    //       ],
+    //       "headers": {
+    //           "Reply-To": "admin@universidadeparavida.com.br"
+    //       },
+    //       "async": true,
+    //       "ip_pool": "Main Pool",
+    //   }
+    //   }
+    // }
+    // return await Axios.request(config).then(success => console.log(success.data)).catch(err => console.log(err))
   }
 }
 
-const Class = new nodeMailer()
+let madrill = new Mandrill()
 
-Class.configMail()
+madrill.sendEmail()
